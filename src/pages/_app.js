6cac8704e762523fae476d02d797e684c44d3c5a
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-02-27 21:48:30 -0500

    init

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <h1>playing around</h1>
+            <Component {...pageProps} />
+        </>
+    )
+}
