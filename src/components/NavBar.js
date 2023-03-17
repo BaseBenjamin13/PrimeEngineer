1ce228caa1b0c8a2c8c430480db3c20ab9564d87
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-16 22:37:58 -0400

    adding logo to navbar & in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import Image from 'next/image'
 import style from '@/styles/NavBar.module.css'
 
 function NavBar() {
     return (
         <div className={style.nav}>
-            <div>
-                <h1>Logo</h1>
+            <div className={style.navItem}>
+                <Image
+                    src="/logo.png"
+                    width={85}
+                    height={85}
+                    alt="Picture of Prime Engineer logo"
+                />
             </div>
+
             <div className={style.navItem}>
                 <h1 className={style.navItemText}>Books</h1>
             </div>
+
             <div className={style.navItem}>
                 <h1 className={style.navItemText}>Learn &#8681;</h1>
             </div>
+
             <div className={style.navItem}>
                 <h1 className={style.navItemText}>Products</h1>
             </div>
+
             <div className={style.navItem}>
                 <h1 className={style.navItemText}>About</h1>
             </div>
+            
         </div>
     )
 }
