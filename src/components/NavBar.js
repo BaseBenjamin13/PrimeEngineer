6482a0c95a1c689a23140066bf97dbff3878537e
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-17 12:28:50 -0400

    adding links to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import Image from 'next/image'
-import style from '@/styles/NavBar.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+import style from '@/styles/NavBar.module.css';
 
 function NavBar() {
     return (
         <div className={style.nav}>
             <div className={style.navItem}>
-                <Image
-                    src="/logo.png"
-                    width={85}
-                    height={85}
-                    alt="Picture of Prime Engineer logo"
-                />
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        width={85}
+                        height={85}
+                        alt="Picture of Prime Engineer logo"
+                    />
+                </Link>
             </div>
 
             <div className={style.navItem}>
-                <h1 className={style.navItemText}>Books</h1>
+                <Link href="/books">
+                    <h1 className={style.navItemText}>Books</h1>
+                </Link>
             </div>
 
             <div className={style.navItem}>
@@ -23,13 +28,17 @@ function NavBar() {
             </div>
 
             <div className={style.navItem}>
-                <h1 className={style.navItemText}>Products</h1>
+                <Link href="/products">
+                    <h1 className={style.navItemText}>Products</h1>
+                </Link>
             </div>
 
             <div className={style.navItem}>
-                <h1 className={style.navItemText}>About</h1>
+                <Link href="/about">
+                    <h1 className={style.navItemText}>About</h1>
+                </Link>
             </div>
-            
+
         </div>
     )
 }
