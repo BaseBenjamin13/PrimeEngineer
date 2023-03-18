193b75440118c8b4047de16a81b82dcb0ffaf82a
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-17 22:13:50 -0400

    added brand name header to homePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,19 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Prime Engineer</title>
-        <meta name="description" content="Home page of Prime Engineer" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <main className={styles.main}>
-        
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Prime Engineer</title>
+                <meta name="description" content="Home page of Prime Engineer" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/logo.png" />
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.brandName}>
+                    <h1>Prime Engineer</h1>
+                </div>
+            </main>
+        </>
+    )
 }
