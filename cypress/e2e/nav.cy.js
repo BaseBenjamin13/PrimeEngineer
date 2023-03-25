7723246daa43c20ecffa1f1dfb79162366d73971
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-25 17:17:47 -0400

    cypress init & wrote first test-about link sends user to about url: PASSING

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -1,2 +1,12 @@
+// const { describe } = require("eslint/lib/rule-tester/rule-tester");
 
 
+describe('Navigation', () => {
+    it('should navigate to the about page', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.get('a[href*="about"]').click()
+
+        cy.url().should('include', '/about')
+    })
+})
