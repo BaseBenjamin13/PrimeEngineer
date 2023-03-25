a1fa2e74c5ba168f130a233cc16b76f56d4c2d82
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-25 15:03:34 -0400

    added functionality to next button, to render next blog post

--- src/components/BlogSlider.js
+++ src/components/BlogSlider.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css'
 
 function BlogSlider({ blogContent }) {
+
+    const [blogIndex, setBlogIndex] = useState(0)
+
+    const handleChangeBlogIndex = (num) => {
+        if(num === 1 && blogIndex < blogContent.length -1){
+            setBlogIndex(blogIndex + num);
+        }
+    }
+
     return (
         blogContent && 
         <div className={styles.blogContainer}>
             <h1>Main Blog</h1>
-            {blogContent.map((content, index) => {
-                return (
-                    <div key={index}>
-                        <p className={styles.blogContent}>
-                            <span className={styles.blogTitle}>{content.title}</span>
-                            {content.content}
-                        </p>
-                    </div>
-                )
-            })}
+
+            <div>
+                <p className={styles.blogContent}>
+                    <span className={styles.blogTitle}>{blogContent[blogIndex].title}</span>
+                    {blogContent[blogIndex].content}
+                </p>
+            </div>
 
             <div className={styles.blogNavContainer}>
                 <button className={styles.blogBackBtn}>Back</button>
-                <button className={styles.blogNextBtn}>Next</button>
+                <button className={styles.blogNextBtn} onClick={() => handleChangeBlogIndex(1)}>Next</button>
             </div>
 
         </div>
