b91119579fac165cf735d48010ff7bbaa2714700
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-27 15:29:03 -0400

    TEST: should navigate to the products page: PASSING

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -40,6 +40,14 @@ describe('Navigation', () => {
         cy.url().should('include', '/python')
     })
 
+    it('should navigate to the products page', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.get('a[href*="products"]').click()
+
+        cy.url().should('include', '/products')
+    })
+
     it('should navigate to the about page', () => {
         cy.visit('http://localhost:3000/')
 
