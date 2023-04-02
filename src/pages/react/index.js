434df222a3d5d4ece71bebc038e4340142eb32cc
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-04-01 23:27:43 -0400

    learn react page init

---
+++ src/pages/react/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/Learn.module.css'
+
+function LearnReact() {
+  return (
+    <div className={styles.learnReactContainer}>
+        <h1>Learn React</h1>
+    </div>
+  )
+}
+
+export default LearnReact;
