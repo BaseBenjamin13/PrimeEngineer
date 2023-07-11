6781179b25e2520431891dd6535f8d44dd3d1377
Benjamin Morgiewicz <benmorgiewicz@gmail.com>
2023-07-11 15:08:34 -0400

    styling language header component

--- src/components/language/header/index.js
+++ src/components/language/header/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-function Header() {
+import { useRouter } from 'next/router';
+
+function Header(params) {
+
+  const router = useRouter();
+  const { asPath } = useRouter();
+  console.log(asPath)
+
   return (
     <div className={styles.headerContainer}>
+      <div>
         <h1>Blog</h1>
+      </div>
+        <span></span>
+      <div>
         <h1>Videos</h1>
+      </div>
+        <span></span>
+      <div>
         <h1>Quizs</h1>
+      </div>
     </div>
   )
 }
